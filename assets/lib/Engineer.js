fd9e2db4dea5b06be9cd6b4b2c37ef6d2d276984
DJ Hersh <DanielJHersh@gmail.com>
2020-11-27 16:55:22 -0800

    Commented the rest of my js files

--- assets/lib/Engineer.js
+++ assets/lib/Engineer.js
@@ -1,6 +1,6 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 const Employee = require('./Employee');
 
+// Engineer class that creates an object that extends the Employee class and adds engineer specific parameters
 class Engineer extends Employee {
     constructor(name, id, email, github) {
         super(name, id, email);
--- assets/lib/Intern.js
+++ assets/lib/Intern.js
@@ -1,6 +1,6 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
 const Employee = require('./Employee');
 
+// Intern class that creates an object that extends the Employee class and adds intern specific parameters
 class Intern extends Employee {
     constructor(name, id, email, school) {
         super(name, id, email);
